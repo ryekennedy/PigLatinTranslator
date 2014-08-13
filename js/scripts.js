cc032f46a825417e98e42798fa238bc85a2c8ca4
ryekennedy <ryekennedy@gmail.com>
2014-08-13 15:22:37 -0500

    Added functionality for "qu"

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,18 @@ function pigLatinWord(word) {
 	 if(vowels.indexOf(firstletter) !== -1)  {
 	 	return word+'ay';
 	 }
-	 
+	 	 
 	 else {
 	 	var iterations = word.length;
 	 	//var i = 1;
+	 	if (word.charAt(0)==='q' && word.charAt(1)==='u') {
+		word = word.slice(2) + word.charAt(0)+ word.charAt(1);
+		var firstletter = word.charAt(0);
+	 	if(vowels.indexOf(firstletter) !== -1) {
+		word = word+'ay';
+		return word;
+			}
+		}
 	 	for (var i = 1; i < iterations;   i++) {
 	 	word = word.slice(1) + word.charAt(0);
 	 	var firstletter = word.charAt(0);
--- spec/specs.js
+++ spec/specs.js
@@ -5,8 +5,14 @@ describe('pigLatinWord', function() {
 	it('return words with one consonant beginning by moving first consonant to the end and adding ay to the end of contatination', function(){
 		pigLatinWord('jam').should.equal('amjay');
 	});
-	it('return words with two consonant beginning by moving first consonant to the end and adding ay to the end of contatination', function(){
+	it('return words with two consonant beginning by moving first and second consonants to the end and adding ay to the end of contatination', function(){
 		pigLatinWord('bran').should.equal('anbray');
+	});
+	it('return words with multiple consonant beginning by moving multiple consonants to the end and adding ay to the end of contatination', function(){
+		pigLatinWord('cbran').should.equal('ancbray');
+	});
+	it('return words with "qu" beginning by moving "qu" to the end and adding ay to the end of contatination', function(){
+		pigLatinWord('quack').should.equal('ackquay');
 	});
 
 });
